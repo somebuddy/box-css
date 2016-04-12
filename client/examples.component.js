ae0a9fac904b1f7082fe1b685cec7c004b0a2258
Somebuddy <aleksey.drogovozov@gmail.com>
2016-04-12 13:08:04 +0000

    Add page container initial styles

--- client/examples.component.js
+++ client/examples.component.js
@@ -1,8 +1,13 @@
+/* global ng */
+
 (function(app) {
 
   app.ExamplesComponent = ng.core.Component({
     selector: 'box-ui-examples',
-    templateUrl: 'examples.component.html'
+    templateUrl: 'examples.component.html',
+    host: {
+      'class': 'page-content'
+    }
   })
   .Class({
     constructor: function () {}

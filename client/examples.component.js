ebd1415f0be546cf47d29406ea63640970330d3d
Somebuddy <aleksey.drogovozov@gmail.com>
2016-04-26 12:47:30 +0000

    Update navbar examples

--- client/examples.component.js
+++ client/examples.component.js
@@ -4,6 +4,7 @@
   app.ExamplesComponent = ng.core.Component({
     selector: 'box-ui-examples',
     templateUrl: 'examples.component.html',
+    directives: [app.CollapseButtonDirective],
     host: {
       'class': 'page-content'
     }

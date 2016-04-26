9381a17e1272d57c9454e3d2f656ad13b9a21ba9
Somebuddy <aleksey.drogovozov@gmail.com>
2016-04-26 11:44:56 +0000

    Add stub directive for toggling menu button

--- client/examples.component.js
+++ client/examples.component.js
@@ -2,9 +2,27 @@
 
 (function(app) {
 
+  var CollapseButtonDirective = ng.core
+  .Directive({
+      selector: '.collapse-button',
+      host: {
+        '(click)': 'onClick()',
+      },
+      providers: [ng.core.ElementRef]
+    })
+  .Class({
+    constructor: [ng.core.ElementRef, function(elementRef) {
+      var el = elementRef.nativeElement;
+      this.onClick = function () {
+        el.classList.toggle('expand');
+      };
+    }]
+  });
+
   app.ExamplesComponent = ng.core.Component({
     selector: 'box-ui-examples',
     templateUrl: 'examples.component.html',
+    directives: [CollapseButtonDirective],
     host: {
       'class': 'page-content'
     }
